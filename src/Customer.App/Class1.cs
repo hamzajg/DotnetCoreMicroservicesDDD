7e331f1435237c757f91b7583bca8c9f97176bd0
Hamza JGUERIM <hamza@PhoeniciaIT.com>
2019-10-08 14:49:24 +0100

    Working On Tests..

--- src/Customer.App/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Customer.App
-{
-    public class Class1
-    {
-    }
-}
--- src/Services.Common/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Services.Common
-{
-    public class Class1
-    {
-    }
-}
